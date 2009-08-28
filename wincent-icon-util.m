46c2025d17186d92128a7d4297982a969413cb39
Wincent Colaiuta <win@wincent.com>
2009-08-28 12:57:14 +0200

    Add call to objc_startCollectorThread()
    This is done automatically in Cocoa desktop apps, but in Foundation
    tools needs to be done manually.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wincent-icon-util.m
+++ wincent-icon-util.m
@@ -1,22 +1,22 @@
+// wincent-icon-util.m
 //
-//  wincent-icon-util.m
-//  Created by Wincent Colaiuta on 26 July 2007.
+// Copyright 2007-2009 Wincent Colaiuta.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-//  Copyright 2007 Wincent Colaiuta.
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+// system headers
 #import <Foundation/Foundation.h>
+#import <objc/objc-auto.h>
 
 //! Show usage information.
 void usage(void)
@@ -29,6 +29,7 @@ void usage(void)
 
 int main(int argc, const char * argv[])
 {
+    objc_startCollectorThread();
     NSAutoreleasePool   *pool       = [[NSAutoreleasePool alloc] init];
     int                 exitStatus  = EXIT_FAILURE;
     OSErr               err;
