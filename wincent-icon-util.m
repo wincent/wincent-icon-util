1f3c7c0bef29a1fbdc1945e52c7bfdfafb8e6f92
Wincent Colaiuta <win@wincent.com>
2009-08-29 15:22:07 +0200

    Drop unnecessary import statement
    This header is the prefix (precompiled) header so there is no need to
    explicitly import it again.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wincent-icon-util.m
+++ wincent-icon-util.m
@@ -15,7 +15,6 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // system headers
-#import <Foundation/Foundation.h>
 #import <objc/objc-auto.h>
 
 //! Show usage information.
