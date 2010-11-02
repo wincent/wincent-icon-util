684adedfe94a83ae0c57d5935b7eff1f329d1d4f
Wincent Colaiuta <win@wincent.com>
2010-11-02 16:10:51 +0100

    Extract more parts of distro.sh into buildtools
    This commit updates the buildtools submodule from e5ede6c to f3aaa1d:
    
      f3aaa1d Replace ReleaseNotes.sh script with rewrite, git-release-notes.txt
      42269a5 Add git-src-archive.sh
      d00c141 git-check-tag.sh: echo tag for utility by scripts
      42d3f64 Add git-check-tag.sh script
      3bf7829 git-last-tag.sh: handle case where no commits or tags in repo
    
    These are three new scripts that have been extracted from the distro.sh
    script into buildtools for reuse.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- distro.sh
+++ distro.sh
@@ -1,80 +1,38 @@
 #!/bin/sh -e
-# Copyright 2009 Wincent Colaiuta.
+# Copyright 2009-2010 Wincent Colaiuta. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
-. "buildtools/Common.sh"
-
-# print warning if current HEAD is not a signed/annotated tag
-TAGGED=""
-
-# for signed tags $HEAD will be "2.0" or similar
-# for unsigned tags or untagged commits will be "2.0-5-g08867ea" or similar
-HEAD=$(git describe)
-for TAG in $(git tag)
-do
-  if [ $TAG = $HEAD ]; then
-    TAGGED=$TAG
-  fi
-done
-if [ -z "$TAGGED" ]; then
-  warn "current HEAD is not tagged/annotated, but it should be for official releases"
-  TAGGED=$HEAD
-fi
+TAGGED=$(buildtools/git-check-tag.sh)
 
 # give installer package unique name prior to uploading
 mv "$BUILT_PRODUCTS_DIR/WincentIconUtility.pkg" \
    "$BUILT_PRODUCTS_DIR/$PROJECT-$TAGGED.pkg"
 
 # prep source archive
-git archive $TAGGED > "$BUILT_PRODUCTS_DIR/$PROJECT-$TAGGED-src.tar"
-
-# add submodules to archive
-git ls-tree $TAGGED | grep '^160000 ' | \
-while read mode type sha1 path
-do
-  rm -rf "$PROJECT_TEMP_DIR/$PROJECT-$TAGGED-$path-src/$path"
-  mkdir -p "$PROJECT_TEMP_DIR/$PROJECT-$TAGGED-$path-src/$path"
-  (cd $path && git archive $sha1 | tar -xf - -C "$PROJECT_TEMP_DIR/$PROJECT-$TAGGED-$path-src/$path")
-  tar -rf "$BUILT_PRODUCTS_DIR/$PROJECT-$TAGGED-src.tar" \
-       -C "$PROJECT_TEMP_DIR/$PROJECT-$TAGGED-$path-src" $path
-done
-bzip2 -f "$BUILT_PRODUCTS_DIR/$PROJECT-$TAGGED-src.tar"
+buildtools/git-src-archive.sh
 
 # prep release notes
-PREV_TAG=$($BUILDTOOLS_DIR/git-last-tag.sh)
-test -n "$PREV_TAG" || die 'no value for PREV_TAG'
-
-NOTES="$BUILT_PRODUCTS_DIR/$PROJECT-$TAGGED-release-notes.txt"
-DETAILED_NOTES="$BUILT_PRODUCTS_DIR/$PROJECT-$TAGGED-detailed-release-notes.txt"
-echo "Changes from $PREV_TAG to $TAGGED:" > $NOTES
-echo "" >> $NOTES
-git log --no-decorate --pretty=format:'    %s' $PREV_TAG..$TAGGED >> $NOTES
-
-echo "Changes from $PREV_TAG to $TAGGED:" > $DETAILED_NOTES
-echo "" >> $DETAILED_NOTES
-git log --no-decorate $PREV_TAG..$TAGGED >> $DETAILED_NOTES
-
-# append submodule notes
-git ls-tree $TAGGED | grep '^160000 ' | \
-while read mode type sha1 path
-do
-  LAST_REF=$(git ls-tree $PREV_TAG | grep '^160000 ' | grep $path | awk '{ print $3 }')
-  SUBMODULE_NOTES=$(cd $path && git log --no-decorate --pretty=format:'    %s' $LAST_REF..$sha1)
-  DETAILED_SUBMODULE_NOTES=$(cd $path && git log --no-decorate $LAST_REF..$sha1)
-  if [ $(echo "$SUBMODULE_NOTES" | wc -l) -ne 1 ]; then
-    echo "" >> $NOTES
-    echo "" >> $NOTES
-    echo "$path (submodule): " >> $NOTES
-    echo "" >> $NOTES
-    echo "$SUBMODULE_NOTES" >> $NOTES
-
-    echo "" >> $DETAILED_NOTES
-    echo "--------------------------------------------------------------------------------" >> $DETAILED_NOTES
-    echo "" >> $DETAILED_NOTES
-    echo "$path (submodule): " >> $DETAILED_NOTES
-    echo "" >> $DETAILED_NOTES
-    echo "$DETAILED_SUBMODULE_NOTES" >> $DETAILED_NOTES
-  fi
-done
+buildtools/git-release-notes.sh
 
 # prep plaintext version of manpage
 MANPAGE="wincent-icon-util.1"
