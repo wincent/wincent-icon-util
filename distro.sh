b4a32dd9edc1cf20c2b0ede380d29fa9c87c2bfd
Wincent Colaiuta <win@wincent.com>
2010-11-02 12:19:45 +0100

    Update distro.sh to produce correct submodule release notes
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- distro.sh
+++ distro.sh
@@ -40,27 +40,39 @@ done
 bzip2 -f "$BUILT_PRODUCTS_DIR/$PROJECT-$TAGGED-src.tar"
 
 # prep release notes
-NOTES="$BUILT_PRODUCTS_DIR/$PROJECT-$TAGGED-release-notes.txt"
-"$BUILDTOOLS_DIR/ReleaseNotes.sh" > $NOTES
-git ls-tree $TAGGED | grep '^160000 ' | \
-while read mode type sha1 path
-do
-  SUBMODULE_NOTES=$(cd $path && "$BUILDTOOLS_DIR/ReleaseNotes.sh" --tag-prefix="wincent-icon-util-")
-  if [ $(echo "$SUBMODULE_NOTES" | wc -l) -ne 1 ]; then
-    echo -n "$path: " >> $NOTES
-    echo "$SUBMODULE_NOTES" >> $NOTES
-  fi
-done
+PREV_TAG=$($BUILDTOOLS_DIR/git-last-tag.sh)
+test -n "$PREV_TAG" || die 'no value for PREV_TAG'
 
-NOTES="$BUILT_PRODUCTS_DIR/$PROJECT-$TAGGED-detailed-release-notes.txt"
-"$BUILDTOOLS_DIR/ReleaseNotes.sh" --long > $NOTES
+NOTES="$BUILT_PRODUCTS_DIR/$PROJECT-$TAGGED-release-notes.txt"
+DETAILED_NOTES="$BUILT_PRODUCTS_DIR/$PROJECT-$TAGGED-detailed-release-notes.txt"
+echo "Changes from $PREV_TAG to $TAGGED:" > $NOTES
+echo "" >> $NOTES
+git log --no-decorate --pretty=format:'    %s' $PREV_TAG..$TAGGED >> $NOTES
+
+echo "Changes from $PREV_TAG to $TAGGED:" > $DETAILED_NOTES
+echo "" >> $DETAILED_NOTES
+git log --no-decorate $PREV_TAG..$TAGGED >> $DETAILED_NOTES
+
+# append submodule notes
 git ls-tree $TAGGED | grep '^160000 ' | \
 while read mode type sha1 path
 do
-  SUBMODULE_NOTES=$(cd $path && "$BUILDTOOLS_DIR/ReleaseNotes.sh" --long --tag-prefix="wincent-icon-util-")
+  LAST_REF=$(git ls-tree $PREV_TAG | grep '^160000 ' | grep $path | awk '{ print $3 }')
+  SUBMODULE_NOTES=$(cd $path && git log --no-decorate --pretty=format:'    %s' $LAST_REF..$sha1)
+  DETAILED_SUBMODULE_NOTES=$(cd $path && git log --no-decorate $LAST_REF..$sha1)
   if [ $(echo "$SUBMODULE_NOTES" | wc -l) -ne 1 ]; then
-    echo -n "$path: " >> $NOTES
+    echo "" >> $NOTES
+    echo "" >> $NOTES
+    echo "$path (submodule): " >> $NOTES
+    echo "" >> $NOTES
     echo "$SUBMODULE_NOTES" >> $NOTES
+
+    echo "" >> $DETAILED_NOTES
+    echo "--------------------------------------------------------------------------------" >> $DETAILED_NOTES
+    echo "" >> $DETAILED_NOTES
+    echo "$path (submodule): " >> $DETAILED_NOTES
+    echo "" >> $DETAILED_NOTES
+    echo "$DETAILED_SUBMODULE_NOTES" >> $DETAILED_NOTES
   fi
 done
 
