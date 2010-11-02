f88192fc5392847d5a0aea793faf3784a2ef066f
Wincent Colaiuta <win@wincent.com>
2010-11-02 19:14:04 +0100

    tag-release.sh: no longer tag submodules
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- tag-release.sh
+++ tag-release.sh
@@ -29,18 +29,9 @@ test -z "$PREFIX" || die "not at top of working tree"
 
 echo "Preview:"
 echo "  git tag -s $VERSION -m \"$VERSION release\" in $(pwd)"
-(cd buildtools && echo "  git tag -s \"${TAG_NAME}-${VERSION}\" -m \"$TAG_NAME $VERSION release\" in $(pwd)")
 
 /bin/echo -n "Enter yes to continue: "
 read CONFIRM
 if [ "$CONFIRM" = "yes" ]; then
   git tag -s $VERSION -m "$VERSION release"
-  cd buildtools
-  git tag -s "${TAG_NAME}-${VERSION}" -m "$LONG_TAG_NAME $VERSION release"
-  cd -
 fi
-
-echo "NOTE:"
-echo "  You have added a tag within the buildtools submodule;"
-echo "  you will need to 'git push --tags' in order for that tag to be"
-echo "  shared with other repositories."
