332dd2207eccff7c3bcac5c4a37b12ff88fa7605
Wincent Colaiuta <win@wincent.com>
2010-11-02 09:39:21 +0100

    Make code signing conditional
    Only try to sign if the executable is not already signed, and only if
    the user is "wincent".
    
    Seeing as this is open source project, this change means that other
    people can produce (unsigned) builds without errors or warnings.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- sign.sh
+++ sign.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-if [ "$BUILD_STYLE" = 'Release' ]; then
-  codesign -s wincent.com "$CODESIGNING_FOLDER_PATH"
+if [ "$BUILD_STYLE" = 'Release' -a "$USER" = 'wincent' ]; then
+  codesign -v "$CODESIGNING_FOLDER_PATH" 2> /dev/null ||
+    codesign -s wincent.com "$CODESIGNING_FOLDER_PATH"
 fi
